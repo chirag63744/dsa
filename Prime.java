8ae77d12da28a8a3f12757e4a200e88f4dd4a112
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-09-27 21:34:44 +0530

    Display n

--- Prime.java
+++ Prime.java
@@ -1,17 +1,17 @@
-public class Solution {
-    public static String isPrime(int num) {
-        int n = (int) Math.sqrt(num);
-        int count = 0;
-        for (int i = 2; i <= n; i++) {
-            if (num % i == 0) {
-                count = count + 1;
-                break;
-            }
-        }
-        if (count == 0&&num!=1) {
-            return "YES";
-        } else {
-            return "NO";
-        }
-    }
-}
+// public class Solution {
+// public static String isPrime(int num) {
+// int n = (int) Math.sqrt(num);
+// int count = 0;
+// for (int i = 2; i <= n; i++) {
+// if (num % i == 0) {
+// count = count + 1;
+// break;
+// }
+// }
+// if (count == 0&&num!=1) {
+// return "YES";
+// } else {
+// return "NO";
+// }
+// }
+// }
---
+++ display_n.java
@@ -0,0 +1,13 @@
+// public class Solution {
+// public static int[] printNos(int x) {
+// // Write Your Code Here
+// int arr[]=new int[x];
+// for(int i=0;i<x;i++)
+// {
+// arr[i]=i+1;
+
+// }
+// return arr;
+
+// }
+// }
