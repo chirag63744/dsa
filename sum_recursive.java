e40166963ac5f8675bb9b7ecfc39bf31123c1597
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-09-29 21:37:51 +0530

    Sum_recursive

---
+++ sum_recursive.java
@@ -0,0 +1,19 @@
+// public class Solution {
+// private static long sum = 0;
+// private static long i = 1;
+
+// public static long sumFirstN(long n) {
+// sum = n; // Reset sum and i for each new calculation.
+// i = 1;
+// Rec(n);
+// return sum;
+// }
+
+// private static void Rec(long n) {
+// if (i < n) {
+// sum = sum + i;
+// i++;
+// Rec(n); // Recursive call
+// }
+// }
+// }
