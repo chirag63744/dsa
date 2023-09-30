028a0783faedabd1ec0fa558ffd83250ae3405e9
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-09-30 16:04:11 +0530

    reverse and fact

---
+++ n_factorial.java
@@ -0,0 +1,16 @@
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class Solution {
+// public static List<Long> factorialNumbers(long n) {
+// List<Long> l = new ArrayList<>();
+// long sum = 1;
+// for (int i = 1; sum <= n; i++) {
+// sum *= i;
+// if (sum <= n) {
+// l.add(sum);
+// }
+// }
+// return l;
+// }
+// }
---
+++ reverse_array.java
@@ -0,0 +1,13 @@
+// public class Solution {
+// public static int[] reverseArray(int n, int []nums) {
+// int s=n-1;
+// int[]num=new int[n];
+// for(int i=0;i<n;i++)
+// {
+// num[i]=nums[s];
+// s=s-1;
+// }
+// return num;
+
+// }
+// }
