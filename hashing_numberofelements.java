4eeef45576a98aeddb197d80b9c984677468792e
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-03 22:06:49 +0530

    Hashing

---
+++ hashing_numberofelements.java
@@ -0,0 +1,13 @@
+// public class Solution {
+// public static int[] countFrequency(int n, int x, int[] nums) {
+// int[] hash = new int[n];
+
+// for (int i = 0; i < n; i++) {
+// int num = nums[i];
+// if (num >= 1 && num <= n) {
+// hash[num - 1]++;// isme values store kar rahe h 0 se.
+// }
+// }
+// return hash;
+// }
+// }
