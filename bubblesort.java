39b170c7c9df16c7ff1fa8e1efc7b7d8562f0aae
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-07 22:23:42 +0530

    bubble sort

---
+++ bubblesort.java
@@ -0,0 +1,26 @@
+
+// public class Solution {
+// public static void bubbleSort(int[] arr, int n) {
+// int count=0;
+// for(int i =n-1;i>=0;i--)
+// {
+// for(int j= 0;j<=i-1;j++)
+// {
+// if(arr[j]>arr[j+1])
+// {
+// int temp=arr[j];
+// arr[j]=arr[j+1];
+// arr[j+1]=temp;
+// count=count+1;
+// }
+
+// }
+// if(count==0)
+// {
+// break;
+// }
+// }
+// //Your code goes here
+// }
+
+// }
