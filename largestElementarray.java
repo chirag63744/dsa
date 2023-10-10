9053b8dc28d62fcec8102426e5a8afa82575fc24
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-10 23:44:14 +0530

    largestElement

---
+++ largestElementarray.java
@@ -0,0 +1,20 @@
+// import java.util.* ;
+// import java.io.*;
+
+// public class Solution {
+
+// static int largestElement(int[] arr, int n) {
+// // Write your code here.
+// int count=arr[0];
+// for(int i =1;i<n;i++)
+// {
+// if(count<arr[i])
+// {
+// count=arr[i];
+
+// }
+// }
+// return count;
+
+// }
+// }
