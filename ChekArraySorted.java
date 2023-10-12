3c2a0886b3dd619b0a2dcffce7056a8587b3a850
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-12 18:54:00 +0530

    CheckArraySorted

---
+++ ChekArraySorted.java
@@ -0,0 +1,21 @@
+// public class Solution {
+// public static int isSorted(int n, int []a) {
+// // Write your code here.
+// int cp=0;
+// for(int i=0;i<n-1;i++)
+// {
+// if(a[i]>a[i+1])
+// {
+// cp=1;
+// break;
+// }
+// }
+// if(cp==0)
+// {
+// return 1;
+// }
+// else {
+// return 0;
+// }
+// }
+// }
