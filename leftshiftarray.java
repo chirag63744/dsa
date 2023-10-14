cf6ebc4f619fee82a2da9effc5d960c1077519c6
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-14 21:58:20 +0530

    leftshiftarray

---
+++ leftshiftarray.java
@@ -0,0 +1,22 @@
+// import java.util.* ;
+// import java.io.*;
+
+// public class Solution {
+
+// static int[] rotateArray(int[] arr, int n) {
+// // Write your code here.
+// int []aa=new int[n];
+// for(int i=0;i<n;i++)
+// {
+// if(i==0)
+// {
+// aa[n-1]=arr[i];
+
+// }
+// else{
+// aa[i-1]=arr[i];
+// }
+// }
+// return aa;
+// }
+// }
