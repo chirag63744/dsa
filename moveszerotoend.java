d7d03a7f28cb2b13747f83c84952bcd306065eee
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-15 23:36:24 +0530

    moves0toend

---
+++ moveszerotoend.java
@@ -0,0 +1,31 @@
+// public class Solution {
+// public static int[] moveZeros(int n, int []a) {
+// // Write your code here.
+// int count =n-1;
+// int count2=0;
+// int b[]=new int[n];
+// for(int i =0;i<n;i++)
+// {
+// if(a[i]==0)
+// {
+// if(count==n)
+// {
+// b[count]=a[i];
+// count = count -1;
+
+// }
+// else{
+// b[count]=a[i];
+// count =count-1;
+
+// }
+// }
+// else{
+// b[count2]=a[i];
+// count2=count2+1;
+
+// }
+// }
+// return b;
+// }
+// }
