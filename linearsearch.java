f5a9f8c2c79f56c34e94a9b6f276f95ab69b5c42
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-16 18:27:40 +0530

    linear search

---
+++ linearsearch.java
@@ -0,0 +1,23 @@
+// import java.util.*;
+// public class Solution {
+// public static int linearSearch(int n, int num, int []arr){
+// // Write your code here.
+// int num2=0;
+// int count=0;
+// for(int i=0;i<n;i++)
+// {
+// if(arr[i]==num)
+// {
+// num2=i;
+// count=1;
+// break;
+// }
+// }
+// if(num2==0 && count==0)
+// {
+// return num2-1;
+// }
+// return num2;
+
+// }
+// }
