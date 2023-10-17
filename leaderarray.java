22718f16e55bd3cba29c4fd86a04c8d231ac8273
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-17 18:43:50 +0530

    leaderarray

---
+++ leaderarray.java
@@ -0,0 +1,26 @@
+// import java.util.*;
+// public class Solution {
+// public static List< Integer > superiorElements(int []a) {
+// List<Integer> as=new ArrayList<Integer>();
+// int sum=0;
+// int n= a.length;
+// // Write your code here.
+// for(int i =n-1;i>=0;i--)
+// {
+// if(i==n-1)
+// {
+// as.add(a[i]);
+// sum=a[i];
+// }
+// else{
+// if(a[i]>sum)
+// {
+// as.add(a[i]);
+// sum=a[i];
+// }
+
+// }
+// }
+// return as;
+// }
+// }
