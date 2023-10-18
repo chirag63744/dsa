65c0fa7be0f56cbdfafbfbbfc02e670eb51d09ea
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-18 14:51:51 +0530

    blockswap

---
+++ blockSwap.java
@@ -0,0 +1,16 @@
+// class Solution {
+// public void rotate(int[] nums, int k) {
+// int n= nums.length;
+// int l=1;
+// int [] nums2=new int[n];
+// for(int i=0;i<n;i++)
+// {
+// nums2[(i+k)%n]=nums[i];
+// }
+// for(int i=0;i<n;i++)
+// {
+// nums[i]=nums2[i];
+// }
+
+// }
+// }
