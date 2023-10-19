b00aecf031c3a630f3c2816e1ce5a5cc3496f1b8
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-19 22:37:34 +0530

    MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,16 @@
+// public class Solution {
+// public static int missingNumber(int []a, int N) {
+// // Write your code here.
+// int d=0;
+// for(int i=0;i<=N-1;i++)
+// {
+// d=a[i]+d;
+
+// }
+// int sum=N*(N+1)/2;
+// int r=sum-d;
+
+// return r;
+
+// }
+// }
