113722fec6bb9c35a21657d668649bcd7dbeb789
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-22 20:52:30 +0530

    TargetSum

---
+++ TargetSum.java
@@ -0,0 +1,30 @@
+// public class Solution {
+// public static String read(int n, int []book, int target){
+// // Write your code here.
+// int sum=0;
+// String s="NO";
+// for(int i =0;i<n-1;i++)
+// {
+// int j=i+1;
+// sum=book[i];
+// while(sum<target)
+// {
+// if(j<n)
+// {
+// sum=book[j]+sum;
+// if(sum==target);
+// {
+// s="YES";
+// break;
+// }
+
+// }
+// else{
+// break;
+// }
+// }
+// }
+
+// return s;
+// }
+// }
