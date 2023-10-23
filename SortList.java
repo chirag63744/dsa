aff5fb031d5fb3db74a37821e553a7e96fff0d17
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-23 22:12:52 +0530

    SortList

---
+++ SortList.java
@@ -0,0 +1,9 @@
+// import java.util.* ;
+// import java.io.*;
+// public class Solution {
+// public static void sortArray(ArrayList<Integer> arr, int n) {
+
+// // Write your code here.
+// Collections.sort(arr);
+// }
+// }
