349abdcbdb04dbde184e9d577d4e9af394f46bcd
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-27 23:02:19 +0530

    palindrome

---
+++ palindromme.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+
+// public class Solution {
+
+// public static boolean checkPalindrome(String str) {
+// // Remove non-alphanumeric characters and convert to lowercase
+// str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+// // Check if the string is equal to its reverse
+// return str.equals(new StringBuilder(str).reverse().toString());
+// }
