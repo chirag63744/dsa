c71a52ef19ca84349b58a53eb1fe672d0ac931e5
rokzc <chirag.garg2020a@vitstudent.ac.in>
2023-10-28 23:58:13 +0530

    vowels

---
+++ vowels_in_string.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+
+// public class Solution {
+// public static Boolean splitString(String str) {
+// int n = str.length() / 2;
+// int count1 = 0;
+// int count2 = 0;
+
+// for (int i = 0; i < n; i++) {
+// char ch1 = Character.toLowerCase(str.charAt(i));
+// char ch2 = Character.toLowerCase(str.charAt(i + n));
+
+// if (ch1 == 'a' || ch1 == 'e' || ch1 == 'i' || ch1 == 'o' || ch1 == 'u') {
+// count1++;
+// }
+
+// if (ch2 == 'a' || ch2 == 'e' || ch2 == 'i' || ch2 == 'o' || ch2 == 'u') {
+// count2++;
+// }
+// }
+
+// return count1 == count2;
+// }
+// }
