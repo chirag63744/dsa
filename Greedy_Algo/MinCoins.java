6d42e875fdfb401639875890a47faa9031cbf9da
rokzc <chirag.garg2020a@vitstudent.ac.in>
2024-03-11 17:35:43 +0530

    Mincoins

---
+++ Greedy_Algo/MinCoins.java
@@ -0,0 +1,25 @@
+// import java.util.ArrayList;
+// import java.util.List;
+// public class Solution {
+// public static List<Integer> MinimumCoins(int n) {
+// // Write your code here.
+// List <Integer> fin=new ArrayList<>();
+// int []arr=new int[]{1000,500,100,50,20,10,5,2,1};
+// for(int i =0;i<9;i++)
+// {
+// if(arr[i]<=n)
+// {
+// fin.add(arr[i]);
+// n=n-arr[i];
+// while(n>=arr[i]){fin.add(arr[i]);
+// n=n-arr[i];}
+
+// }
+// if(n==0)
+// {
+// break;
+// }
+// }
+// return fin;
+// }
+// }
